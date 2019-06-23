78feefc88f1d3ef6f5d984f1228edacbc31198a2
mikesinc <mas.sinclair@gmail.com>
2019-06-23 14:18:09 +0800

    heroku setup

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,10 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })      
-    .catch(err => res.status(400).json('unable to register'));
+    .catch(err => {
+        res.status(400).json('unable to register')
+        console.log(err)
+    });
 }
 
 module.exports = {
