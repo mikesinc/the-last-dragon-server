e51272205931f8c0f6ac99fd731481d0e6a083ca
mikesinc <mas.sinclair@gmail.com>
2019-08-02 18:14:30 +0800

    url change

--- controllers/templates.js
+++ controllers/templates.js
@@ -3,11 +3,11 @@ module.exports = {
   confirm: id => ({
     subject: 'The Last Dragon - Confirmation Email',
     html: `
-      <a href='https://limitless-beach-12582.herokuapp.com/confirm/${id}'>
+      <a href='https://the-last-dragon.herokuapp.com/confirm/${id}'>
         click to confirm email
       </a>
     `,      
-    text: `Copy and paste this link: https://limitless-beach-12582.herokuapp.com/confirm/${id}`
+    text: `Copy and paste this link: https://the-last-dragon.herokuapp.com/confirm/${id}`
   })
   
 }
