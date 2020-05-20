7597427d6d6f5188ae42bc9527701de564d5abfe
mikesinc <mas.sinclair@gmail.com>
2020-05-20 23:41:03 +0800

    confirm fix

--- controllers/confirm.js
+++ controllers/confirm.js
@@ -1,56 +1,45 @@
-const msgs = require('./msgs');
-const templates = require('./templates');
-const sendEmail = require('./send');
+const msgs = require("./msgs");
+const templates = require("./templates");
+const sendEmail = require("./send");
 
 exports.collectEmail = (db) => (req, res) => {
-    const { email } = req.body
+  const { email } = req.body;
 
-    db.select('id', 'email', 'verified').from('users')
-        .where('email', '=', email)
-        .then(user => {
-            // console.log(user);
-            if (!user) {
-                return res.status(400).send("User not found!");
-            }
-
-            else if (user && !user.verified) {
-                sendEmail(user[0].email, templates.confirm((user[0].id)))
-                    .then(() => res.json({ msg: msgs.resend }))
-            }
-
-            else {
-                res.json({ msg: msgs.alreadyConfirmed })
-            }
-
-        })
-        .catch(err => res.status(400).send(err.detail));
-}
+  db.select("id", "email", "verified")
+    .from("users")
+    .where("email", "=", email)
+    .then((user) => {
+      if (user.verified === false) {
+        console.log("nah");
+        sendEmail(user[0].email, templates.confirm(user[0].id)).then(() =>
+          res.json({ msg: msgs.resend })
+        );
+      } else if (user.verified === true) {
+        res.json({ msg: msgs.alreadyConfirmed });
+      }
+      return res.status(400).send("User not found!");
+    })
+    .catch((err) => res.status(400).send(err.detail));
+};
 
 exports.confirmEmail = (db) => (req, res) => {
-    const { id } = req.params
-    db.select('id', 'verified').from('users')
-        .where('id', '=', id)
-        .then(user => {
+  const { id } = req.params;
+  db.select("id", "verified")
+    .from("users")
+    .where("id", "=", id)
+    .then((user) => {
+      if (user.verified === false) {
+        db.table("users")
+          .where({ id: id })
+          .update({ verified: true }, ["id", "verified"])
+          .then((updatedRows) => {
+            updatedRows === [{ id: id, verified: true }];
+          })
+          .catch((err) => res.status(400).send(err.detail));
+      } else if (user.verified === true) {
+        res.json({ msg: msgs.alreadyConfirmed });
+      }
 
-            if (!user) {
-                res.json({ msg: msgs.couldNotFind })
-            }
-
-            else if (user && !user.verified) {
-                db.table('users')
-                    .where({ id: id })
-                    .update({ verified: true }, ['id', 'verified'])
-                    .then((updatedRows) => {
-                        updatedRows === [{ id: id, verified: true }]
-                    })
-                    .catch(err => res.status(400).send(err.detail))
-            }
-
-            else {
-                res.json({ msg: msgs.alreadyConfirmed })
-            }
-
-        })
-}
-
-
+      return res.json({ msg: msgs.couldNotFind });
+    });
+};
