f0c3a60a27abbd06ba23c1f78cc3673156f6ceb2
mikesinc <mas.sinclair@gmail.com>
2020-05-21 00:19:53 +0800

    email fix

--- controllers/templates.js
+++ controllers/templates.js
@@ -3,11 +3,11 @@ module.exports = {
   confirm: id => ({
     subject: 'The Last Dragon - Confirmation Email',
     html: `
-      <a href='https://the-last-dragon.netlify.app/confirm/${id}'>
+      <a href='https://the-last-dragon.netlify.app/confirmed/${id}'>
         click to confirm email
       </a>
     `,      
-    text: `Copy and paste this link: https://the-last-dragon.netlify.app/confirm/${id}`
+    text: `Copy and paste this link: https://the-last-dragon.netlify.app/confirmed/${id}`
   })
   
 }
